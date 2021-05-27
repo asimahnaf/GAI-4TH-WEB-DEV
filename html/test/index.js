5f633f3c2f684b7d0ef4174ec7c9e6ec5d6436a4
ahnaf <63817873+asimahnaf@users.noreply.github.com>
2021-05-27 23:30:23 +0600

    Add files via upload

---
+++ html/test/index.js
@@ -0,0 +1,16 @@
+console.log("hello");
+let name="yoko";
+let age='1';
+console.log(age);
+console.log(name);
+
+//array 
+let array=[];
+array=['apple','1', 'orange'];
+console.log(array);
+let pen={
+    type:'bollpen',
+    color:'black',
+    cost:13
+};
+console.log(pen.type);
